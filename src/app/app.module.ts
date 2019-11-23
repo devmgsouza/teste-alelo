fdef3390c559e63b1aebb7f50caa74a41415fb0b
Marcio Galvão <dev.mgsouza@gmail.com>
2019-11-23 00:47:41 -0300

    adicionado jquery ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { StorageService } from './core/storage.service';
 import { LoaderInterceptor } from './core/loader-interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { HomeComponent } from './components/home/home.component';
-
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
